aad61cd9479ad160ec3ab13f054feb00d7b67f12
dodch <dotchadam@gmail.com>
2026-01-31 16:07:12 +0100

    Add firebase config for GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// WARNING: Add 'firebase-config.js' to your .gitignore file to prevent leaking this key.
+// Ensure this API key is restricted to your domains in the Google Cloud Console.
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTz-7xkhelFZZUgAX6Qdc_sppiRzLDfhA",
+  authDomain: "dodch-96b15.firebaseapp.com",
+  projectId: "dodch-96b15",
+  storageBucket: "dodch-96b15.appspot.com",
+  messagingSenderId: "253879203711",
+  appId: "1:253879203711:web:879893218eb835e1bc0c4a",
+  measurementId: "G-4WJBPKNT1H"
+};
